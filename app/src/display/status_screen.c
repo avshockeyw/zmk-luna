933cc6c74ae785bcb97fb140c5268e86fd42b4e5
Pitchaya Thitiwimol <pitchaya.t@gmail.com>
2022-09-06 01:05:58 +0700

    add luna widget

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -9,6 +9,7 @@
 #include <zmk/display/widgets/battery_status.h>
 #include <zmk/display/widgets/layer_status.h>
 #include <zmk/display/widgets/wpm_status.h>
+#include <zmk/display/widgets/luna_status.h>
 #include <zmk/display/status_screen.h>
 
 #include <logging/log.h>
@@ -32,6 +33,10 @@ static struct zmk_widget_layer_status layer_status_widget;
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
 static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LUNA_STATUS)
+static struct zmk_widget_luna_status luna_status_widget;
 #endif
 
 lv_obj_t *zmk_display_status_screen() {
@@ -70,6 +75,11 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
     lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -12,
                  0);
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LUNA_STATUS)
+    zmk_widget_luna_status_init(&luna_status_widget, screen);
+    lv_obj_align(zmk_widget_luna_status_obj(&luna_status_widget), NULL, LV_ALIGN_CENTER, 0, 0);
 #endif
     return screen;
 }
